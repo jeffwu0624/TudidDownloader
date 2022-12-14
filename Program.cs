cabc3cc164ecf83ed50890579831fde7b64ca0ee
JeffWu0624 <jeffwu0624@hotmail.com>
2022-12-15 00:20:19 +0800

    feat: export excel file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Playwright;
+
+var playwright = await Playwright.CreateAsync();
+
+var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
+{
+    Headless = false, 
+    SlowMo = 10000
+});
+
+var page = await browser.NewPageAsync();
+
+await page.GotoAsync("https://udid.fda.gov.tw/ManageEquipmentSearch.aspx");
+
+await page.Locator("[popup_open='popup-1']").ClickAsync();
+await page.Locator("#ContentPlaceHolder1_Button5").ClickAsync();
