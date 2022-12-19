6f03d5cebd407810ae6612288e80f0544f158e98
JeffWu0624 <jeffwu0624@hotmail.com>
2022-12-20 00:30:04 +0800

    feat: add timeout

--- Program.cs
+++ Program.cs
@@ -5,12 +5,21 @@ var playwright = await Playwright.CreateAsync();
 var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
 {
     Headless = false, 
-    SlowMo = 10000
+    //SlowMo = 10000
 });
 
 var page = await browser.NewPageAsync();
 
-await page.GotoAsync("https://udid.fda.gov.tw/ManageEquipmentSearch.aspx");
+// add timeout
+page.SetDefaultTimeout(3000000);
 
-await page.Locator("[popup_open='popup-1']").ClickAsync();
-await page.Locator("#ContentPlaceHolder1_Button5").ClickAsync();
+await page.GotoAsync("https://udid.fda.gov.tw/ManageEquipmentSearch.aspx");
+var download = await page.RunAndWaitForDownloadAsync(async () =>
+{
+    await page.Locator("[popup_open='popup-1']").ClickAsync();
+    await page.Locator("#ContentPlaceHolder1_Button5").ClickAsync();
+});
+
+Console.WriteLine(await download.PathAsync());
+//await page.Locator("[popup_open='popup-1']").ClickAsync();
+//await page.Locator("#ContentPlaceHolder1_Button5").ClickAsync();
